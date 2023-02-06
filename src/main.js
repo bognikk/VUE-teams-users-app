3b1c23a4c9865dc6facc991b3aeca39903825b9a
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-06 12:14:35 -0500

    passing data with route params

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList }, //our-domain.com/teams => TeamsList
-    { path: '/users', component: UsersList }, //our-domain.com/teams => UsersList
+    { path: '/users', component: UsersList },
+    { path: '/teams/:teamId', component: TeamMembers }, // this rout will be active if user enters /teams/anythigAfterThat
+    // { path: '/teams/new' }, // this this is a different scenarion from one above
   ],
   linkActiveClass: 'active-router', // change from router-link-active
 });
